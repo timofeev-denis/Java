060f6616d0cddba5d5227629dc2cb6ab9e7d8f5d
Denis Timofeev <nakolenke@gmail.com>
2019-03-27 19:40:59 +0300

    WIP

---
+++ mockserver/src/main/java/ru/code4fun/learn/mockserver/MockserverApplication.java
@@ -0,0 +1,13 @@
+package ru.code4fun.learn.mockserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MockserverApplication.class, args);
+    }
+
+}
---
+++ mockserver/src/test/java/ru/code4fun/learn/mockserver/MockServerTest.java
@@ -0,0 +1,16 @@
+package ru.code4fun.learn.mockserver;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest // (classes = Application.class)
+@AutoConfigureMockMvc
+public class MockServerTest {
+
+    @Test
+    void getContentFromTheWebTest() {
+
+    }
+}
