bcb8b8457bb3d620a48cda3dffd8607d8ba53a0c
Denis Timofeev <nakolenke@gmail.com>
2019-03-29 20:28:27 +0300

    Add new project

---
+++ filter-status-code/src/main/java/ru/code4fun/demo/filterstatuscode/FilterStatusCodeApplication.java
@@ -0,0 +1,13 @@
+package ru.code4fun.demo.filterstatuscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilterStatusCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilterStatusCodeApplication.class, args);
+	}
+
+}
